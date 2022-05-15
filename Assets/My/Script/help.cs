e7f581e6fb2a2c17cb4479d5fb2c35280041b17b
Emerson-Foco <emerson.importante@gmail.com>
2022-05-15 14:20:43 -0300

    add new button help

---
+++ Assets/My/Script/help.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class help : MonoBehaviour
+{
+    //Variables:
+    public bool rightAnswer = true;
+    public Gameplay gameplay;
+    public int cont = 0;
+    public int contLimit = 2;
+    public GameObject[] iconHelp;
+
+    public void Help()
+    {
+        if(cont <= contLimit)
+        {
+            print("true");
+            gameplay = GameObject.Find("Player").GetComponent<Gameplay>();
+            gameplay.score = gameplay.score - 10;
+            
+            iconHelp[cont].gameObject.SetActive(false);
+            cont++;
+
+            gameplay.NextQuestion();
+        }
+    }
+}
